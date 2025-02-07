e75ddfc19d458179a46fad2539a112d5212c2674
Luiz Felipe <felipefelizatti215@gmail.com>
2025-02-07 18:32:05 -0300

    Update index.tsx

--- src/pages/terms/index.tsx
+++ src/pages/terms/index.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 export default function TermsOfUse() {
   return (
-    <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif', display: 'flex', flexDirection: 'column', color: 'white' }}>
+    <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif', display: 'flex', flexDirection: 'column', color: 'white', gap: 16 }}>
       <h1>Terms of Use</h1>
 
       <h2>1. Acceptance of Terms</h2>
       <p>
-        By accessing and using [Your App Name] (&quot;the App&quot;), you accept and agree to be bound by the terms and provision of this agreement. In addition, when using this App&apos;s particular services, you shall be subject to any posted guidelines or rules applicable to such services.
+        By accessing and using Mobile Template (&quot;the App&quot;), you accept and agree to be bound by the terms and provision of this agreement. In addition, when using this App&apos;s particular services, you shall be subject to any posted guidelines or rules applicable to such services.
       </p>
 
       <h2>2. Description of Service</h2>
       <p>
-        [Your App Name] is a [brief description of your app&apos;s purpose and functionality]. You understand and agree that the Service is provided &quot;as-is&quot; and that [Your Company Name] assumes no responsibility for the timeliness, deletion, misdelivery, or failure to store any user communications or personalization settings.
+        Mobile Template is a [brief description of your app&apos;s purpose and functionality]. You understand and agree that the Service is provided &quot;as-is&quot; and that Company assumes no responsibility for the timeliness, deletion, misdelivery, or failure to store any user communications or personalization settings.
       </p>
 
       <h2>3. User Obligations</h2>
@@ -22,17 +22,17 @@ export default function TermsOfUse() {
 
       <h2>4. Modifications to Service</h2>
       <p>
-        [Your Company Name] reserves the right to modify or discontinue, temporarily or permanently, the Service (or any part thereof) with or without notice. You agree that [Your Company Name] shall not be liable to you or to any third party for any modification, suspension, or discontinuance of the Service.
+        Company reserves the right to modify or discontinue, temporarily or permanently, the Service (or any part thereof) with or without notice. You agree that Company shall not be liable to you or to any third party for any modification, suspension, or discontinuance of the Service.
       </p>
 
       <h2>5. Termination</h2>
       <p>
-        You agree that [Your Company Name], in its sole discretion, may terminate your password, account (or any part thereof), or use of the Service, and remove and discard any Content within the Service, for any reason, including, without limitation, for lack of use or if [Your Company Name] believes that you have violated or acted inconsistently with the letter or spirit of the Terms of Service.
+        You agree that Company, in its sole discretion, may terminate your password, account (or any part thereof), or use of the Service, and remove and discard any Content within the Service, for any reason, including, without limitation, for lack of use or if Company believes that you have violated or acted inconsistently with the letter or spirit of the Terms of Service.
       </p>
 
       <h2>6. Links</h2>
       <p>
-        The Service may provide, or third parties may provide, links to other World Wide Web sites or resources. Because [Your Company Name] has no control over such sites and resources, you acknowledge and agree that [Your Company Name] is not responsible for the availability of such external sites or resources and does not endorse and is not responsible or liable for any Content, advertising, products, or other materials on or available from such sites or resources.
+        The Service may provide, or third parties may provide, links to other World Wide Web sites or resources. Because Company has no control over such sites and resources, you acknowledge and agree that Company is not responsible for the availability of such external sites or resources and does not endorse and is not responsible or liable for any Content, advertising, products, or other materials on or available from such sites or resources.
       </p>
 
       <h2>7. Disclaimer of Warranties</h2>
@@ -40,13 +40,13 @@ export default function TermsOfUse() {
         You expressly understand and agree that:
         <ul>
           <li>Your use of the Service is at your sole risk. The Service is provided on an &quot;as is&quot; and &quot;as available&quot; basis.</li>
-          <li>[Your Company Name] makes no warranty that (i) the Service will meet your requirements, (ii) the Service will be uninterrupted, timely, secure, or error-free, (iii) the results that may be obtained from the use of the Service will be accurate or reliable, (iv) the quality of any products, services, information, or other material purchased or obtained by you through the Service will meet your expectations, and (v) any errors in the software will be corrected.</li>
+          <li>Company makes no warranty that (i) the Service will meet your requirements, (ii) the Service will be uninterrupted, timely, secure, or error-free, (iii) the results that may be obtained from the use of the Service will be accurate or reliable, (iv) the quality of any products, services, information, or other material purchased or obtained by you through the Service will meet your expectations, and (v) any errors in the software will be corrected.</li>
         </ul>
       </p>
 
       <h2>8. Limitation of Liability</h2>
       <p>
-        You expressly understand and agree that [Your Company Name] shall not be liable for any direct, indirect, incidental, special, consequential, or exemplary damages, including but not limited to, damages for loss of profits, goodwill, use, data, or other intangible losses (even if [Your Company Name] has been advised of the possibility of such damages), resulting from: (i) the use or the inability to use the Service; (ii) the cost of procurement of substitute goods and services resulting from any goods, data, information, or services purchased or obtained or messages received or transactions entered into through or from the Service; (iii) unauthorized access to or alteration of your transmissions or data; (iv) statements or conduct of any third party on the Service; or (v) any other matter relating to the Service.
+        You expressly understand and agree that Company shall not be liable for any direct, indirect, incidental, special, consequential, or exemplary damages, including but not limited to, damages for loss of profits, goodwill, use, data, or other intangible losses (even if Company has been advised of the possibility of such damages), resulting from: (i) the use or the inability to use the Service; (ii) the cost of procurement of substitute goods and services resulting from any goods, data, information, or services purchased or obtained or messages received or transactions entered into through or from the Service; (iii) unauthorized access to or alteration of your transmissions or data; (iv) statements or conduct of any third party on the Service; or (v) any other matter relating to the Service.
       </p>
 
       <h2>9. Governing Law</h2>
